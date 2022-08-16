31af704bb5d585c35a9b19aacc9597001c15c22f
longpt271 <longpt271@gmail.com>
2022-08-16 21:26:48 +0700

    Xử lý "Esc" Keypress Event

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 'use strict';
 
-//------------ Làm việc với Class
+//------------ Xử lý "Esc" Keypress Event
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -22,6 +20,17 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-// dùng closeModal thay vì closeModal()
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//--- Xử lý "Esc"
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  // console.log('A key was pressed');
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    console.log('Esc was pressed');
+
+    closeModal();
+  }
+});
