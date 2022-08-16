fc6ddb7bd3a8e0e797a479ea19b04f7f8b9aac8f
longpt271 <longpt271@gmail.com>
2022-08-16 21:26:31 +0700

    Làm việc với Class

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 'use strict';
 
+//------------ Làm việc với Class
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+// dùng closeModal thay vì closeModal()
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
